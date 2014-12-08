474e4cdb2b1fc002b1dda979b9a4295a4aa72eef
tertius42 <louthomas316@gmail.com>
2014-12-08 10:05:12 -0500

    Just a git test.

--- laelius.pas
+++ laelius.pas
@@ -1,33 +1,54 @@
 {
-   laelius.pas
-   
-   Copyright 2014 Louis Thomas <lthomas@mail.swvgs.us>
-   
-   This program is free software; you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
-   the Free Software Foundation; either version 2 of the License, or
-   (at your option) any later version.
-   
-   This program is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU General Public License for more details.
-   
-   You should have received a copy of the GNU General Public License
-   along with this program; if not, write to the Free Software
-   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
-   MA 02110-1301, USA.
-   
-   
+	laelius.pas
+
+	Copyright 2014 Louis Thomas <lthomas@mail.swvgs.us>
+	Sponser: Mr. Rick Fisher
+
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 2 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+	MA 02110-1301, USA.
+
+	Purpose: Essentially, look for a virus in a file.
 }
 
 
 program laelius;
 
-uses crt;
-var i : byte;
-
+uses crt, sysutils;
+const
+	ver = 0.1;
+var
+	i: byte;
+	dir: String;
+	t: byte;
+	isUnix: boolean;
 BEGIN
-		
+	clrscr;//clear the terminal
+	
+	dir := getCurrentDir; //get directory and determine OS
+	if Copy(dir,1,1) = '/' then
+		isUnix := true
+	else isUnix := false;
+	
+	if isUnix then //add a slash at the end of the directory
+		dir := dir + '/'
+	else dir := dir + '\';
+	
+	writeln('Laelius ',ver:2:1); //announce program and version
+  writeln('This program comes with ABSOLUTELY NO WARRANTY.');
+	writeln('This is free software, and you are welcome to redistribute it');
+  writeln('under certain conditons.');
+	
+	
 END.
-
